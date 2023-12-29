0bd54d7d1c329dfd7b6d5102fd144e67a20d1045
Yash <yashmohan3@gmail.com>
2023-12-29 16:44:48 +0530

    Enable susequent clicks on button

--- script.js
+++ script.js
@@ -11,13 +11,23 @@ function moveButton() {
     const noButton = document.querySelector('#buttonContainer button:last-child');
 
     // Reset button position
+    noButton.style.transition = 'none'; // Disable transition for instant reset
     noButton.style.transform = 'translateX(0)';
+    void noButton.offsetWidth; // Trigger reflow
+    noButton.style.transition = ''; // Re-enable transition
 
     // Calculate random position within a limited range
     const minMove = -50; // Minimum movement to the left
     const maxMove = 50; // Maximum movement to the right
     const randomMove = Math.floor(Math.random() * (maxMove - minMove + 1)) + minMove;
 
-    noButton.style.transform = `translateX(${randomMove}px)`;
-    noButton.disabled = true;
+    // Move the button after a short delay to allow the transition to reset
+    setTimeout(() => {
+        noButton.style.transform = `translateX(${randomMove}px)`;
+    }, 10);
+
+    // Re-enable the button after the transition ends
+    noButton.addEventListener('transitionend', () => {
+        noButton.disabled = false;
+    }, { once: true });
 }
